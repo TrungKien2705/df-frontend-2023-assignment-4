aa5f961baa746bcdb7d8e8eee4f3c33bff3fa465
TrungKien2705 <nt.kien.2705@gmail.com>
2023-10-06 23:45:53 +0700

    update

--- src/app/book/[id]/layout.tsx
+++ src/app/book/[id]/layout.tsx
@@ -1,26 +1,26 @@
 import React from 'react'
-import { Metadata } from 'next'
-import { getBookById } from '../../../services/bookServices'
+// import { Metadata } from 'next'
+// import { getBookById } from '../../../services/bookServices'
 
-type Props = {
-  params: { id: number }
-}
-
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const { id } = params
-
-  const book = await getBookById(id)
-
-  return {
-    title: book?.name || 'Not Found',
-    description: book?.description || 'description',
-    openGraph: {
-      images:
-        book?.thumbnail ||
-        'https://trungkien-assignment-4.vercel.app/assets/Assignment-4.png',
-    },
-  }
-}
+// type Props = {
+//   params: { id: number }
+// }
+//
+// export async function generateMetadata({ params }: Props): Promise<Metadata> {
+//   const { id } = params
+//
+//   const book = await getBookById(id)
+//
+//   return {
+//     title: book?.name || 'Not Found',
+//     description: book?.description || 'description',
+//     openGraph: {
+//       images:
+//         book?.thumbnail ||
+//         'https://trungkien-assignment-4.vercel.app/assets/Assignment-4.png',
+//     },
+//   }
+// }
 
 interface LayoutProps {
   children: React.ReactNode
